98dd73bcc410ea294f3e306174ecc6d070bcce5e
mikicvi <vilim.mikic@gmail.com>
2023-11-17 15:22:37 +0000

    app chg, ip

--- app.py
+++ app.py
@@ -8,10 +8,10 @@ app = Flask(__name__)
 CORS(app)
 # My SQL Instance configurations
 # Change these details to match your instance configurations
-app.config['MYSQL_USER'] = 'A'
-app.config['MYSQL_PASSWORD'] = 'B'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'secret'
 app.config['MYSQL_DB'] = 'student'
-app.config['MYSQL_HOST'] = 'db.mydomain.ie'
+app.config['MYSQL_HOST'] = '20.56.24.122'
 mysql.init_app(app)
 
 @app.route("/add") #Add Student
