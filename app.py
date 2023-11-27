172d03509e33c2eb8a2a4974607f017dd8771004
mikicvi <88291034+mikicvi@users.noreply.github.com>
2023-11-27 16:22:12 +0000

    routing fix

--- app.py
+++ app.py
@@ -27,10 +27,9 @@ def execute_query(query):
 
 @app.route("/add", methods=['POST'])  # Add Student
 def add():
+    name = request.args.get('name')
+    email = request.args.get('email')
     try:
-        name = request.args.get('name')
-        email = request.args.get('email')
-
         query = '''INSERT INTO students(studentName, email) VALUES('{}', '{}');'''.format(name, email)
         success = execute_query(query)
 
